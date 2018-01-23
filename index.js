ec638bb39d7b0dd5f02dbea76fb53aaf9a042257
mystvearn <nguyenduyanh1991@gmail.com>
2018-01-23 11:18:12 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
-﻿$(function () {
-    //Widgets count
-    $('.count-to').countTo();
+var data = [], totalPoints = 110;
+function getRandomData() {
+    if (data.length > 0) data = data.slice(1);
 
-    //Sales count to
-    $('.sales-count-to').countTo({
-        formatter: function (value, options) {
-            return '$' + value.toFixed(2).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, ' ').replace('.', ',');
-        }
-    });
+    while (data.length < totalPoints) {
+        var prev = data.length > 0 ? data[data.length - 1] : 50, y = prev + Math.random() * 10 - 5;
+        if (y < 0) { y = 0; } else if (y > 100) { y = 100; }
 
-    initRealTimeChart();
-    initDonutChart();
-    initSparkline();
-});
+        data.push(y);
+    }
+
+    var res = [];
+    for (var i = 0; i < data.length; ++i) {
+        res.push([i, data[i]]);
+    }
+
+    return res;
+}
 
 var realtime = 'on';
 function initRealTimeChart() {
@@ -95,21 +98,22 @@ function initDonutChart() {
     });
 }
 
-var data = [], totalPoints = 110;
-function getRandomData() {
-    if (data.length > 0) data = data.slice(1);
+$(function () {
+    //Widgets count
+    $('.count-to').countTo();
 
-    while (data.length < totalPoints) {
-        var prev = data.length > 0 ? data[data.length - 1] : 50, y = prev + Math.random() * 10 - 5;
-        if (y < 0) { y = 0; } else if (y > 100) { y = 100; }
+    //Sales count to
+    $('.sales-count-to').countTo({
+        formatter: function (value, options) {
+            return '$' + value.toFixed(2).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, ' ').replace('.', ',');
+        }
+    });
 
-        data.push(y);
-    }
+    initRealTimeChart();
+    initDonutChart();
+    initSparkline();
+});
 
-    var res = [];
-    for (var i = 0; i < data.length; ++i) {
-        res.push([i, data[i]]);
-    }
 
-    return res;
-}
+
+
